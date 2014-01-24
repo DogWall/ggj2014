3268107a2f43e116c918b926b970828e20e260f2
cgoessen <christophe.goessen@gmail.com>
2014-01-24 23:25:19 +0100

    init 2 le retour

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 
 //init enchant.js
+
 enchant();
 
-window.onload = Game;
 
 /**
  * Main
  */
-function Game () {
+window.onload = function () {
 
     var game = new Core(window.innerWidth / 2 , window.innerHeight / 2); //screen res
     game.fps = 24;
-    game.preload(/*'foo.png','bar.png'*/); //preload assets png, wav etc
-    game.score = 0;
+    game.preload("img/jeanjacques.png"/*'foo.png','bar.png'*/); //preload assets png, wav etc
+    game.onload = function(){
+    ground = new Sprite(200,358);
+    console.log("sprite ok")
+    ground.image=game.assets["img/jeanjacques.png"];
+    ground.x=0;
+    ground.y=0;
+    ground.frame=0;
+        //ground.tl.moveBy(200,0,60,enchant.Easing.LINEAR)
+        ground.tl.delay(10).then(function (){ground.frame=1}).delay(10).then(function (){ground.frame=2}).delay(10).then(function (){ground.frame=1}).delay(10).then(function (){ground.frame=0}).loop();
+    game.rootScene.addChild(ground);
+    }
 
     // var bear = new Sprite(32, 32);
     // bear.image = game.assets['chara1.png'];
